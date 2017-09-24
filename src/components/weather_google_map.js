303a0eb7b5ab1ec93b6336792066784ed507a7c4
M Gosschalk <monica.gosschalk@btinternet.com>
2017-09-24 15:42:22 +0100

    added google maps

--- src/components/weather_google_map.js
+++ src/components/weather_google_map.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 
 class GoogleMap extends Component {
 
-  // componentDidMount() {
-  //   new google.maps.Map(this.refs.map, {
-  //     zoom: 12,
-  //     center: {
-  //       lat: this.props.lat,
-  //       lng: this.props.lon
-  //     }
-  //   });
-  // }
+  componentDidMount() {
+    new window.google.maps.Map(this.refs.map, {
+      zoom: 12,
+      center: {
+        lat: this.props.lat,
+        lng: this.props.lon
+      }
+    });
+  }
 
   render() {
     // this.refs.map
--- src/components/weather_list.js
+++ src/components/weather_list.js
@@ -22,7 +22,7 @@ class WeatherList extends Component {
 
     return (
       <tr key={name}>
-        <td>{name}<GoogleMap lon={lon} lat={lat}/></td>
+        <td><GoogleMap lon={lon} lat={lat}/></td>
         <td><Chart data={temps} color="orange" units="C" /></td>
         <td><Chart data={pressures} color="green" units="hPa" /></td>
         <td><Chart data={humidities} color="blue" units="%" /></td>
